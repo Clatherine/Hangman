c211152c1edb3955e439c09ccf0874d062e7a1a3
Catherine Pulman <159820305+Clatherine@users.noreply.github.com>
2024-06-16 23:14:06 +0100

    have run build

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base:'/fe-tiny-react-apps/Hangman',
+  base:'/',
   plugins: [react()],
-  // build: {
-  //   outDir: 'dist'
-  // }
+  build: {
+    outDir: 'dist'
+  }
 })
