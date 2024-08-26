d0a4b4a29844bc03696d09136c70ec559d6b0d3b
Catherine Pulman <159820305+Clatherine@users.noreply.github.com>
2024-08-26 16:36:16 +0100

    enabled functionality for difficulty setting to come in when game is reset

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-const [word, setWord] =useState("SPECTACULAR")
-const [hangmanState, setHangmanState] = useState(0);
-const [wordState, setWordState] = useState([
+  const [word, setWord] = useState("SPECTACULAR");
+  const [hangmanState, setHangmanState] = useState(0);
+  const [inAction, setInAction] = useState(false);
+  const [wordState, setWordState] = useState([
     "_",
     "_",
     "_",
@@ -18,33 +19,47 @@ const [wordState, setWordState] = useState([
     "_",
   ]);
 
-const handleDifficultyChange=(chosenWord)=>{
-setWord(chosenWord)
-setHangmanState(0)
-setWordState(Array(chosenWord.length).fill("_"))
-const letters = document.getElementsByClassName("letterbutton");
-const lettersArr = [...letters];
-lettersArr.forEach((letter) => {
-  letter.classList.remove("red");
-  letter.classList.remove("green");
-  letter.disabled = false;
-});
-}
+  const handleDifficultyChange = (chosenWord) => {
+    setWord(chosenWord);
+    setHangmanState(0);
+    setWordState(Array(chosenWord.length).fill("_"));
+    const letters = document.getElementsByClassName("letterbutton");
+    const lettersArr = [...letters];
+    lettersArr.forEach((letter) => {
+      letter.classList.remove("red");
+      letter.classList.remove("green");
+      letter.disabled = false;
+    });
+  };
 
   return (
-    <section>
-    <Game  handleDifficultyChange = {handleDifficultyChange}/>
-      <Buttons hangmanState = {hangmanState} setHangmanState = {setHangmanState} wordState = {wordState} word={word} setWordState={setWordState}/>
-
+    <section id="whole">
+      <Game
+        handleDifficultyChange={handleDifficultyChange}
+        inAction={inAction} setInAction={setInAction}
+      /> {
+        inAction?    <Buttons
+        hangmanState={hangmanState}
+        setHangmanState={setHangmanState}
+        wordState={wordState}
+        word={word}
+        setWordState={setWordState}
+        setInAction={setInAction} inAction={inAction}
+      /> : null
+      }
+   
     </section>
   );
 }
 
-
-function Buttons({word, wordState, setWordState, hangmanState, setHangmanState}) {
-
-
-  
+function Buttons({
+  word,
+  wordState,
+  setWordState,
+  hangmanState,
+  setHangmanState,
+  setInAction, inAction
+}) {
   const alphabet = [
     "a",
     "b",
@@ -75,55 +90,62 @@ function Buttons({word, wordState, setWordState, hangmanState, setHangmanState})
   ];
 
   const handleClick = (event) => {
+    console.log(hangmanState, "hangmanstate");
+    console.log(inAction, 'inAction')
+    setInAction(true);
     if (hangmanState < 11) {
-        if (word.includes(event.target.innerHTML)) {
-          console.log("the letter is in the word!");
-          event.target.classList.add("green");
-          setWordState(() => {
-            const updatedWordArr = [...wordState];
-            for (let i = 0; i < word.length; i++) {
-              if (word[i] === event.target.innerHTML) {
-                updatedWordArr[i] = event.target.innerHTML;
-              }
+      if (word.includes(event.target.innerHTML)) {
+        console.log("the letter is in the word!");
+        event.target.classList.add("green");
+        setWordState(() => {
+          const updatedWordArr = [...wordState];
+          for (let i = 0; i < word.length; i++) {
+            if (word[i] === event.target.innerHTML) {
+              updatedWordArr[i] = event.target.innerHTML;
             }
-            if (!updatedWordArr.includes("_")) {
-              console.log("word complete!");
-              setHangmanState("winner");
-            }
-            return updatedWordArr;
-          });
-        } else {
-          console.log("the letter is not in the word!");
-          event.target.classList.add("red");
-          setHangmanState(() => {
-            if (hangmanState < 11) {
-              return hangmanState + 1;
-            } else {
-              return hangmanState;
-            }
-          });
-        }
-        event.target.disabled = true;
+          }
+          if (!updatedWordArr.includes("_")) {
+            console.log("word complete!");
+            setHangmanState("winner");
+          }
+          return updatedWordArr;
+        });
+      } else {
+        console.log("the letter is not in the word!");
+        event.target.classList.add("red");
+        setHangmanState(() => {
+          if (hangmanState < 11) {
+            return hangmanState + 1;
+          } else {
+            return hangmanState;
+          }
+        });
       }
+      
+      event.target.disabled = true;
     }
-
+    else{
+      setInAction(false);
+    }
+  };
 
   return (
     <section id="main">
-    
       <div className="flex-item" id="buttonDiv">
-        <section id = "letterButtons">
-        {alphabet.map((letter) => {
-          return (
-            <button className="letterbutton" onClick={handleClick} key={letter}>
-              {letter.toUpperCase()}
-            </button>
-          );
-        })}
+        <section id="letterButtons">
+          {alphabet.map((letter) => {
+            return (
+              <button
+                className="letterbutton"
+                onClick={handleClick}
+                key={letter}
+              >
+                {letter.toUpperCase()}
+              </button>
+            );
+          })}
         </section>
-        <WordToComplete
-          wordState={wordState}
-        />
+        <WordToComplete wordState={wordState} />
       </div>
       <div className="flex-item" id="hangmanDiv">
         <Hangman hangmanState={hangmanState} />
@@ -132,14 +154,16 @@ function Buttons({word, wordState, setWordState, hangmanState, setHangmanState})
   );
 }
 
-function WordToComplete({  wordState }) {
+function WordToComplete({ wordState }) {
   return (
+   
     <section id="word">
       {wordState.map((letter, index) => {
         return <span key={index}>{letter} </span>;
       })}
     </section>
-  );
+    )
+  
 }
 
 function Hangman({ hangmanState }) {
@@ -150,39 +174,49 @@ function Hangman({ hangmanState }) {
   );
 }
 
-function Game({handleDifficultyChange}) {
-const handleClick = (event) =>{
-
-let chosenWord =""
-const easyArray = ['PICKLE', 'FLOWER', 'TABLE']
-const funArray = ['INTRICATE', 'GRANDPARENT', 'PECULIAR']
-const trickyArray = ['EXUBERANT', 'CZAR', 'SYNDICATE']
-if(event.target.innerHTML ==="Easy"){
-chosenWord = easyArray[Math.floor(Math.random()*easyArray.length)]
-}
-else if (event.target.innerHTML === "Fun") {
-    chosenWord = funArray[Math.floor(Math.random()*funArray.length)]
-  } else if (event.target.innerHTML === "Horribly tricky") {
-    chosenWord = trickyArray[Math.floor(Math.random()*trickyArray.length)]
-  }
-  handleDifficultyChange(chosenWord)
-}
-
+function Game({ handleDifficultyChange, inAction, setInAction }) {
+  const handleClick = (event) => {
+   
+    let chosenWord = "";
+    const easyArray = ["PICKLE", "FLOWER", "TABLE"];
+    const funArray = ["INTRICATE", "GRANDPARENT", "PECULIAR"];
+    const trickyArray = ["EXUBERANT", "CZAR", "SYNDICATE"];
+    if (event.target.innerHTML === "Easy") {
+      chosenWord = easyArray[Math.floor(Math.random() * easyArray.length)];
+    } else if (event.target.innerHTML === "Fun") {
+      chosenWord = funArray[Math.floor(Math.random() * funArray.length)];
+    } else if (event.target.innerHTML === "Horribly tricky") {
+      chosenWord = trickyArray[Math.floor(Math.random() * trickyArray.length)];
+    }
+    handleDifficultyChange(chosenWord);
+    setInAction(true)
+  };
 
   return (
     <section id="header">
       <div id="title">
-      <h1 id="titleText">Hangman</h1>
-      <p id="text">Choose a difficulty level, then guess the word before the man gets hanged!</p>
+        <h1 id="titleText">Hangman</h1>
+        <p id="text">
+          Choose a difficulty level, then guess the word before the man gets
+          hanged!
+        </p>
       </div>
-      <div id="difficulty">
-      <h4> Choose a difficulty level:</h4>
-      <div id="buttons">
-      <button onClick={handleClick} className="game">Easy</button>
-      <button onClick={handleClick}  className="game">Fun</button>
-      <button onClick={handleClick}  className="game">Horribly tricky</button>
-      </div>
-      </div>
+      {inAction ? null : (
+        <div id="difficulty">
+          <h4> Choose a difficulty level:</h4>
+          <div id="buttons">
+            <button onClick={handleClick} className="game">
+              Easy
+            </button>
+            <button onClick={handleClick} className="game">
+              Fun
+            </button>
+            <button onClick={handleClick} className="game">
+              Horribly tricky
+            </button>
+          </div>
+        </div>
+      )}
     </section>
   );
 }
